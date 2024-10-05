948c7a5b83c0f145b65b1a64148abb1ade3815d5
fkh <2420536738@qq.com>
2024-10-05 20:07:43 +0800

    spring和mybatis整合

---
+++ furn-ssm/src/main/java/com/sun/test/T1.java
@@ -0,0 +1,15 @@
+package com.sun.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class T1 {
+    public static void main(String[] args) {
+        //测试spring容器内的bean是否能正常获取
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取bean
+        System.out.println(ioc.getBean("pooledDataSource"));
+        System.out.println(ioc.getBean("sqlSessionFactory"));
+    }
+}
+
